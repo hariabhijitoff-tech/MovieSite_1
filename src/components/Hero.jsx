38fcfd76a032ee45f4a441217f3b108e0c43a314
hariabhijitoff-tech <hariabhijit.off@gmail.com>
2026-01-07 23:21:54 +0530

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 import Search from './Search'
 
@@ -11,3 +12,18 @@ const Hero = ({searchTerm, setSearchTerm}) => {
 }
 
 export default Hero
+=======
+import React from 'react'
+import Search from './Search'
+
+const Hero = ({searchTerm, setSearchTerm}) => {
+  return (
+    <div className='container flex flex-col justify-center items-center text-center h-[60%] w-[80%] '>
+      <h1 className='w-[50%] text-center text-6xl text-white font-bold px-18 pt-28'>Find Your Favourite <span className='text-amber-700'>Movies</span> Without Any Hassle</h1>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+    </div>
+  )
+}
+
+export default Hero
+>>>>>>> 038bbb9 (first commit)
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 
 const Navbar = ({}) => {
@@ -11,3 +12,18 @@ const Navbar = ({}) => {
 }
 
 export default Navbar
+=======
+import React from 'react'
+
+const Navbar = ({}) => {
+  return (
+    <div className=''>
+      <span className='bg-white  text-3xl'>
+        navbar
+        </span>
+    </div>
+  )
+}
+
+export default Navbar
+>>>>>>> 038bbb9 (first commit)
