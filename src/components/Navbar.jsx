c4ec7c26eeb2c457bd6d2f175994b99d7693b899
hariabhijitoff-tech <hariabhijit.off@gmail.com>
2026-07-11 11:33:15 +0530

    Updateed Styles v2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = ({}) => {
   return (
-    <div className='text-white font-bold text-2xl bg-slate-500/70 w-[60%] h-16 rounded-full flex items-center justify-center px-4 sticky top-4 mx-auto z-50 backdrop-blur-2xl'>
-      MOVINOS
+    <div className='text-white font-bold text-4xl bg-slate-800/80 w-full h-16 flex items-center px-4 top-4 mx-auto z-50 backdrop-blur-2xl'>
+      Movinos
     </div>
   )
 }
